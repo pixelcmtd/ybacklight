93a5728b781256789c60cf0d388cbf6e2927e154
chrissx <chrissx@chrissx.ga>
2019-01-29 22:42:14 +0100

    Restructured a bit.

--- ybacklight.c
+++ ybacklight.c
@@ -1,11 +1,8 @@
 #define CONFIG "/etc/ybacklight/dir.conf"
 #define MAX_CFG_LEN 1024
 #define NUM_MAX 16
-#define ROOT 0
 
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
@@ -21,25 +18,11 @@ void load_config(char *bfr)
 		bfr[i] = '\0';
 }
 
-long read_brightness(char *dir)
+long br_read(char *dir, char *fname)
 {
 	char c[MAX_CFG_LEN + 16];
 	strcpy(c, dir);
-	strcat(c, "brightness");
-	char bfr[NUM_MAX];
-	FILE *f = fopen(c, "r");
-	size_t i = fread(bfr, 1, NUM_MAX, f) - 1;
-	fclose(f);
-	if(bfr[i] == '\n')
-		bfr[i] = '\0';
-	return strtol(bfr, 0, 10);
-}
-
-long read_max_brightness(char *dir)
-{
-	char c[MAX_CFG_LEN + 16];
-	strcpy(c, dir);
-	strcat(c, "max_brightness");
+	strcat(c, fname);
 	char bfr[NUM_MAX];
 	FILE *f = fopen(c, "r");
 	size_t i = fread(bfr, 1, NUM_MAX, f) - 1;
@@ -57,21 +40,47 @@ void write_brightness(char *dir, long i)
 	char bfr[NUM_MAX];
 	sprintf(bfr, "%ld\n", i);
 	uid_t uid = getuid();
-	int j = setuid(ROOT);
+	int j = setuid(0); //root
 	int en = errno;
 	FILE *f = fopen(c, "w");
 	if(!f)
 	{
-		printf("No privileges, because");
+		printf("No privileges");
 		if(j)
-			printf(" setting the UID failed: %s\n",
+			printf(", setting the UID failed: %s\n",
 					strerror(en));
 		else
-			printf("...I don't know, maybe this one: %s\n",
+			printf("...I don't know, can't open because errno: %s\n",
 					strerror(errno));
 		return;
 	}
 	fwrite(bfr, 1, strlen(bfr), f);
 	fclose(f);
 	setuid(uid);
+}
+
+int main(int argc, char **argv)
+{
+	char dir[MAX_CFG_LEN];
+	load_config(dir);
+	long i = br_read(dir, "brightness");
+	long j = br_read(dir, "max_brightness");
+	if(argc < 2)
+		printf("%ld/%ld\n", i, j);
+	else if(argc == 2)
+	{
+		if(!strcmp(argv[1], "cur"))
+			printf("%ld\n", i);
+		else if(!strcmp(argv[1], "max"))
+			printf("%ld\n", j);
+		else
+			puts("cur, max, inc [long], dec [long] or set [long]");
+	}
+	else if(!strcmp(argv[1], "inc"))
+		write_brightness(dir, i + strtol(argv[2], 0, 0));
+	else if(!strcmp(argv[1], "dec"))
+		write_brightness(dir, i - strtol(argv[2], 0, 0));
+	else if(!strcmp(argv[1], "set"))
+		write_brightness(dir, strtol(argv[2], 0, 0));
+	return 0;
 }
