7458efa1d2d1ea2aa665d08e75b5f3c942fbbe1a
pixel <chrissx@chrissx.de>
2021-09-14 00:58:01 +0200

    various improvements
    - made the `die` invocation simpler
    - switched from `long` to `long long` for brightness values
    - spaces indent everywhere

--- main.c
+++ main.c
@@ -7,17 +7,16 @@
 #else
 #define debug(f, a)
 #endif
-#include <unistd.h>
 
 int S = 0, w = 0, f = 0;
-void run(char cmd, long *cur, long *max, long arg)
+void run(char cmd, long long *cur, long long *max, long long arg)
 {
 	debug("Cmd: %c; ", cmd);
 	debug("Short: %d\n", *S);
 	switch(cmd)
 	{
-		case 'c': printf("%ld", S ? *cur / SHORT_FACTOR : *cur); w = 1; break;
-		case 'm': printf("%ld", S ? *max / SHORT_FACTOR : *max); w = 1; break;
+		case 'c': printf("%lld", S ? *cur / SHORT_FACTOR : *cur); w = 1; break;
+		case 'm': printf("%lld", S ? *max / SHORT_FACTOR : *max); w = 1; break;
 		case 'S': S = 1; return;
 		case 'i': *cur += S ? arg * SHORT_FACTOR : arg; f = 1; break;
 		case 'd': *cur -= S ? arg * SHORT_FACTOR : arg; f = 1; break;
@@ -29,34 +28,29 @@ void run(char cmd, long *cur, long *max, long arg)
 
 int main(int argc, char **argv)
 {
-#ifdef __OPENBSD__
-        if(pledge("stdio unveil")) die("pledge: %s\n");
-        if(unveil(BACKLIGHT, "rw")) die("unveil: %s\n");
-        if(unveil(NULL, NULL)) die("unveil: %s\n");
-#endif
-	long cur = read_brightness(BRIGHTNESS);
-	long max = read_brightness(MAX_BRIGHTNESS);
-	char c, buf[NUM_MAX], q /*queued*/ = '\0';
-	long a = -1;
-	memset(buf, 0, NUM_MAX);
-	for(int i = 1; i < argc; i++)
-	{
-		char *cp = argv[i];
-		while((c = *cp++))
-			if(isnum(c)) buf[strlen(buf)] = c;
-			else
-			{
-				if(*buf) a = strtol(buf, 0, 10),
-					 memset(buf, 0, NUM_MAX);
-				if(q) run(q, &cur, &max, a),
-                                      q = 0;
-				if((c == 's' || c == 'd' || c == 'i')
-				   && isnum(*(cp + 1))) q = c;
-				else run(c, &cur, &max, a);
-			}
-	}
-	if(strlen(buf) && q) run(q, &cur, &max, strtol(buf, 0, 10));
-	if(w) putchar('\n');
-	if(f) write_brightness(BRIGHTNESS, cur);
-	return 0;
+        long long cur = read_brightness(BRIGHTNESS);
+        long long max = read_brightness(MAX_BRIGHTNESS);
+        char c, buf[NUM_MAX], q /*queued*/ = '\0';
+        long long a = -1;
+        memset(buf, 0, NUM_MAX);
+        for(int i = 1; i < argc; i++)
+        {
+                char *cp = argv[i];
+                while((c = *cp++))
+                        if(isnum(c)) buf[strlen(buf)] = c;
+                        else
+                        {
+                                if(*buf) a = strtoll(buf, 0, 10),
+                                        memset(buf, 0, NUM_MAX);
+                                if(q) run(q, &cur, &max, a),
+                                        q = 0;
+                                if((c == 's' || c == 'd' || c == 'i')
+                                                && isnum(*(cp + 1))) q = c;
+                                else run(c, &cur, &max, a);
+                        }
+        }
+        if(strlen(buf) && q) run(q, &cur, &max, strtoll(buf, 0, 10));
+        if(w) putchar('\n');
+        if(f) write_brightness(BRIGHTNESS, cur);
+        return 0;
 }
--- ybacklight.c
+++ ybacklight.c
@@ -6,27 +6,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#define die(msg) printf(msg, strerror(errno)), exit(1);
+#define die(msg) printf(msg ": %s\n", strerror(errno)), exit(1);
 
-long read_brightness(char *fname)
+long long read_brightness(char *fname)
 {
-	char bfr[NUM_MAX];
-	FILE *f = fopen(fname, "r");
-        if(!f) die("fopen: %s\n");
-	bfr[fread(bfr, 1, NUM_MAX, f) - 1] = '\0';
-	fclose(f);
-	return strtol(bfr, 0, 10);
+        char bfr[NUM_MAX];
+        FILE *f = fopen(fname, "r");
+        if(!f) die("fopen");
+        bfr[fread(bfr, 1, NUM_MAX, f) - 1] = '\0';
+        fclose(f);
+        return strtoll(bfr, 0, 10);
 }
 
-void write_brightness(char *fname, long i)
+void write_brightness(char *fname, long long i)
 {
 	char bfr[NUM_MAX];
-	size_t cnt = sprintf(bfr, "%ld\n", i);
+	size_t cnt = sprintf(bfr, "%lld\n", i);
 	uid_t uid = getuid();
-	if(uid && setuid(0)) die("setuid(0): %s\n");
+	if(uid && setuid(0)) die("setuid(0)");
 	FILE *f = fopen(fname, "w");
-	if(!f) die("fopen: %s\n");
+	if(!f) die("fopen");
 	fwrite(bfr, 1, cnt, f);
 	fclose(f);
-	if(uid && setuid(uid)) die("setuid(uid): %s\n");
+	if(uid && setuid(uid)) die("setuid(uid)");
 }
